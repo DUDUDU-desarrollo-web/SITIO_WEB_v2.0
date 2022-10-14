c0b741eeada42faf92217db021b4feb8b8dc748f
Danielfercj <daniel-7463@live.com>
2022-10-14 14:23:19 -0500

    Orden invertido trabajos, console.log eliminado

--- routes/portafolio.js
+++ routes/portafolio.js
@@ -5,8 +5,6 @@ var db = require('../connection/connection-mysql');
 /* GET portafolio page (portafolio.jade). */
 router.get('/', function(req, res, next) {
     db.query("SELECT * FROM portafolio", function(err,datos){
-        console.log(datos);
-
         res.render('portafolio', { title: 'PORTAFOLIO',trabajos: datos });
     });
   });

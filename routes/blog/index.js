4229193d148bace6308eec15f05837b1985c7b65
Danielfercj <daniel-7463@live.com>
2022-10-17 16:14:50 -0500

    Intento de automatizar  post fallido, será manual

--- routes/blog/index.js
+++ routes/blog/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../../connection/connection-mysql');
 
 /* GET blog page (blog.jade). */
 router.get('/', function(req, res, next) {
@@ -8,7 +9,21 @@ router.get('/', function(req, res, next) {
 
 /* GET 1er post (lo-primero-que-necesitas-para-tener-un-sitio-web.jade). */
 router.get('/lo-primero-que-necesitas-para-tener-un-sitio-web', function(req, res, next) {
-  res.render('blog/lo-primero-que-necesitas-para-tener-un-sitio-web', { title: 'Lo-primero-que-necesitas-para-tener-un-sitio-web' });
+  db.query("SELECT * FROM blog", function(err,datos){
+    console.log(datos);
+    res.render('blog/lo-primero-que-necesitas-para-tener-un-sitio-web', { title: 'Lo primero que necesitas para tener un sitio web',post: datos });
+  });
 });
+
+//Platilla para los demás posts.
+/* GET n post (titulo.jade). */
+/*
+router.get('/titulo-con-guiones', function(req, res, next) {
+  db.query("SELECT * FROM blog", function(err,datos){
+    console.log(datos);
+    res.render('blog/titulo-con-guiones', { title: 'titulo de la pagina',post: datos });
+  });
+});
+*/
 
 module.exports = router;
